7d9f6621dde61dd1fc5784450066fda15d66b3c5
Gustavo Marques <gustavoufms@gmail.com>
2020-07-27 20:49:52 -0300

    create a new topic

---
+++ discuss/web/controllers/topic_controller.ex
@@ -0,0 +1,15 @@
+defmodule Discuss.TopicController do
+  use Discuss.Web, :controller
+
+  alias Discuss.Topic
+
+  def new(conn, _params) do
+    changeset = Topic.changeset(%Topic{}, %{})
+
+    render conn, "new.html", changeset: changeset
+  end
+
+  def create(conn, %{"topic" => topic}) do
+    changeset = Topic.changeset(%Topic{}, topic)
+  end
+end
